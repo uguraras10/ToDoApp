c1c2885b136665b10985dbccaa1330f80889e031
Ugur Aras <ugur_aras.94@hotmail.com>
2022-10-08 22:37:16 +0300

    Commit 2

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Nketiah");
+            Console.WriteLine("Başka bir ekleme yapıldı uleynn ");
         }
     }
 }
